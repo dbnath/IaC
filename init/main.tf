b624cd20e3ff319281d31a650c869678ecb9f160
Debasish Nath <debasish_nath@epam.com>
2023-12-28 17:28:03 -0500

    Added presentations, IaC for budget

--- init/main.tf
+++ init/main.tf
@@ -50,3 +50,59 @@ resource "azurerm_storage_blob" "maintf" {
   source                 = "main.tf"
   depends_on = [ azurerm_storage_container.storage_container ]
 }
+
+resource "azurerm_monitor_action_group" "monitor_action_group" {
+  name                = "CriticalAlertsAction"
+  short_name          = "p0action"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location = azurerm_resource_group.resource_group.location
+  depends_on = [ azurerm_storage_account.storage_account ]
+
+  arm_role_receiver {
+    name                    = "armroleaction"
+    role_id                 = "de139f84-1756-47ae-9be6-808fbbe84772"
+    use_common_alert_schema = true
+  }
+}
+
+resource "azurerm_consumption_budget_resource_group" "consumption_budget_resource_group" {
+  name = "DnConsumptionResourceGroup"
+  resource_group_id = azurerm_resource_group.resource_group.id
+
+  amount = 10
+  time_grain = "Monthly"
+
+  time_period {
+    start_date = "2023-12-01T00:00:00Z"
+    end_date = "2024-01-28T00:00:00Z"
+  }
+
+  filter {
+    dimension {
+      name = "ResourceId"
+      values = [
+        azurerm_resource_group.resource_group.id
+      ]
+    }
+  }
+  notification {
+    enabled        = true
+    threshold      = 50.0
+    operator       = "EqualTo"
+    threshold_type = "Forecasted"
+
+    contact_emails = [
+      "foo@example.com",
+      "bar@example.com",
+    ]
+
+    contact_groups = [
+      azurerm_monitor_action_group.monitor_action_group.id
+    ]
+
+    contact_roles = [
+      "Owner"
+    ]
+  }
+  depends_on = [ azurerm_monitor_action_group.monitor_action_group ]
+}
